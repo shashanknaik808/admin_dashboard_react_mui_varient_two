c31d5ec4b59806135ba42d1bffc1570f41e536b5
Shashank Naik <shashanknaik808@gmail.com>
2024-04-06 23:46:04 +0530

    Implemented BarChart component with Nivo Bar chart integration and theming

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contacts from "./scenes/contacts";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -37,7 +37,7 @@ function App() {
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
+              <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/geography" element={<Geography />} />  */}
             </Routes>
